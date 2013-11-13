b85fe9a6a8db6fb3b3f1db86689e51bec6bfb5fd
Ulfmann <ulf.portner@googlemail.com>
2013-11-13 23:09:29 +0100

    add main accessors to payout

--- lib/producer_payout.rb
+++ lib/producer_payout.rb
@@ -1,2 +1,10 @@
 class ProducerPayout
+
+  attr_reader :producer
+  attr_reader :range
+
+  def initialize(producer, range)
+    @producer = producer
+    @range    = range
+  end
 end
--- spec/lib/producer_payout_spec.rb
+++ spec/lib/producer_payout_spec.rb
@@ -2,7 +2,12 @@ require 'spec_helper'
 load './lib/producer_payout.rb'
 
 describe ProducerPayout do
-  it 'knows a producer' do
+  let(:producer) { Producer.new(name: 'Test Producer') }
+  let(:range) { Date.parse('01.01.2013')..Date.parse('01.02.2013') }
 
+  it 'knows producer and range' do
+    pp = ProducerPayout.new(producer, range)
+    pp.producer.should eq producer
+    pp.range.should eq range
   end
 end
