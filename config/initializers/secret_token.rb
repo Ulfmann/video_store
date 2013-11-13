7711e2af6da4f03692891b87840b7aabcb613562
Ulfmann <ulf.portner@googlemail.com>
2013-11-13 09:06:11 +0100

    create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VideoStore::Application.config.secret_key_base = '2a34e875e6c62bee46c6531c2df10b74f0fa392806b4077ed2cf846c65426c03238302367e52f04a83730222e230af10ed7df3e221be0eb13f14ae37d0f0df31'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VideoStore::Application.config.session_store :cookie_store, key: '_video_store_session'
