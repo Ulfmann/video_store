3fdb776cf16274981434a6261075dc6985cf51c0
Ulfmann <ulf.portner@googlemail.com>
2013-11-13 12:01:34 +0100

    use association both ways

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   validates_presence_of :name
+
+  has_many :subscriptions
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,15 @@
 require 'spec_helper'
 
 describe User do
+
+  subject { User.create(name: 'Bobby Muster') }
+
   it 'has a name' do
-    User.create(name: "Bobby Muster").should be_valid
+    subject.should be_valid
     User.create().should have(1).error_on :name
+  end
+
+  it 'has subscriptions' do
+    subject.subscriptions << Subscription.create
   end
 end
