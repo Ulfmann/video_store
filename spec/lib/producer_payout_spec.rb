abac2441e92c4af138bf512d1a3343d9e5108660
Ulfmann <ulf.portner@googlemail.com>
2013-11-13 23:14:07 +0100

    refactor

--- spec/lib/producer_payout_spec.rb
+++ spec/lib/producer_payout_spec.rb
@@ -4,10 +4,13 @@ load './lib/producer_payout.rb'
 describe ProducerPayout do
   let(:producer) { Producer.new(name: 'Test Producer') }
   let(:range) { Date.parse('01.01.2013')..Date.parse('01.02.2013') }
+  subject { ProducerPayout.new(producer, range) }
 
   it 'knows producer and range' do
-    pp = ProducerPayout.new(producer, range)
-    pp.producer.should eq producer
-    pp.range.should eq range
+    subject.producer.should eq producer
+    subject.range.should eq range
+  end
+
+  it 'determines the watched videos' do
   end
 end
