66573994c6485cf317c9e584cf46c142572f3d36
Ulfmann <ulf.portner@googlemail.com>
2013-11-14 00:11:02 +0100

    payout considers date_range

--- lib/producer_payout.rb
+++ lib/producer_payout.rb
@@ -7,4 +7,15 @@ class ProducerPayout
     @producer = producer
     @range    = range
   end
+
+  def video_views_for_producer
+    VideoView.
+      where(id: video_ids_for_producer).
+      where(created_at: range).to_a
+  end
+
+  def video_ids_for_producer
+    Video.where(producer_id: producer.id).collect(&:id)
+  end
+
 end
