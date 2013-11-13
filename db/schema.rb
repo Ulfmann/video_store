68737008e8f1d3e7befd2c05a326f92d780ea086
Ulfmann <ulf.portner@googlemail.com>
2013-11-13 20:19:55 +0100

    add producer model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131113105940) do
+ActiveRecord::Schema.define(version: 20131113191246) do
+
+  create_table "producers", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "subscriptions", force: true do |t|
     t.integer "user_id"
