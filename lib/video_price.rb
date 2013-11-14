52b405c3caa786d376632e66bc123321dca2575a
Ulfmann <ulf.portner@googlemail.com>
2013-11-14 17:20:30 +0100

    calculate the price per video_view

---
+++ lib/video_price.rb
@@ -0,0 +1,25 @@
+class VideoViewPrice
+
+  attr_reader :video_view_id
+
+  def get(video_view_id)
+    @video_view_id = video_view_id
+    return value
+  end
+
+  def value
+    subscription.price / views_for_subscription
+  end
+
+  def views_for_subscription
+    VideoView.where(subscription_id: subscription.id).count
+  end
+
+  def subscription
+    @subscription ||= Subscription.find video_view.subscription_id
+  end
+
+  def video_view
+    @video_view ||= VideoView.find(video_view_id)
+  end
+end
