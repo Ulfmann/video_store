4bb5168f6fb39a6e110b665b4f617f6dcdfe8a58
Ulfmann <ulf.portner@googlemail.com>
2013-11-15 11:44:31 +0100

    move logic to producer_payout again

--- lib/producer_payout.rb
+++ lib/producer_payout.rb
@@ -9,6 +9,34 @@ class ProducerPayout
   end
 
   def calculate
-    VideoViewPrice.new(producer.id, range).value
+    amount = 0
+    subscriptions.select do |sub|
+      amount += subscription_view_price(sub)
+      puts amount
+    end
+
+    return amount
+  end
+
+  def video_ids
+    Video.where(producer_id: producer.id).collect(&:id)
+  end
+
+  def subscription_ids
+    VideoView.where(video_id: video_ids, created_at: range).collect(&:subscription_id).uniq
+  end
+
+  def video_view_count(sub)
+    VideoView.where(subscription_id: sub.id, created_at: range).count
+  end
+
+  def subscription_view_price(sub)
+    (sub.price / 4.0) / video_view_count(sub)
+  end
+
+  private
+
+  def subscriptions
+    Subscription.find subscription_ids
   end
 end
--- spec/lib/producer_payout_spec.rb
+++ spec/lib/producer_payout_spec.rb
@@ -2,12 +2,45 @@ require 'spec_helper'
 load './lib/producer_payout.rb'
 
 describe ProducerPayout do
-  let(:producer) { Producer.new(name: 'Test Producer') }
-  let(:range) { Date.parse('01.01.2013')..Date.parse('01.02.2013') }
-  subject { ProducerPayout.new(producer, range) }
+  let(:producer) { Producer.find_by_name('Steve') }
+  let(:range) { Date.parse('01.08.2013')..Date.parse('31.08.2013') }
+  let(:payout) { ProducerPayout.new(producer, range) }
 
   it 'knows producer and range' do
-    subject.producer.should eq producer
-    subject.range.should eq range
+    payout.producer.should eq producer
+    payout.range.should eq range
+  end
+
+  describe '#subscription_ids' do
+    it 'returns subscription ids from watched videos of a producer' do
+      payout.subscription_ids.should =~ [1,2,4]
+    end
+  end
+
+  describe '#video_ids' do
+    it 'returns the video_ids of a producer' do
+      payout.video_ids.should eq [3,4,5]
+    end
+  end
+
+  describe '#video_view_count' do
+    it 'returns the click amount for a subscription' do
+      sub = Subscription.find 2
+      payout.video_view_count(sub).should eq 4
+
+    end
+  end
+
+  describe '#subscription_view_price' do
+    it 'returns a price per view' do
+      sub = Subscription.find 2
+      payout.subscription_view_price(sub).should eq 9.35625
+    end
+  end
+
+  describe '#calculate' do
+    it 'returns the total payout for a producer' do
+      payout.calculate.should eq 20.28
+    end
   end
 end
