0804616797b40ed66a9c5e2222ee5b8288e9f621
Ulfmann <ulf.portner@googlemail.com>
2013-11-15 10:04:39 +0100

    remove specs

--- spec/lib/producer_payout_spec.rb
+++ spec/lib/producer_payout_spec.rb
@@ -10,22 +10,4 @@ describe ProducerPayout do
     subject.producer.should eq producer
     subject.range.should eq range
   end
-
-  context 'Watched Videos' do
-    before do
-      video = FactoryGirl.create(:video, producer: producer)
-      another_video = FactoryGirl.create :video
-      @vv1   = FactoryGirl.create(:video_view, video: video, created_at: Date.parse('01.01.2013'))
-      @vv2   = FactoryGirl.create(:video_view, video: video, created_at: Date.parse('01.03.2013'))
-      @vv2   = FactoryGirl.create(:video_view, video: another_video, created_at: Date.parse('01.03.2013'))
-    end
-
-    context 'during a given time range' do
-      it 'determines the video hits' do
-        subject.video_views_for_producer.should include @vv1
-        subject.video_views_for_producer.should_not include @vv2
-        subject.video_views_for_producer.should_not include @vv3
-      end
-    end
-  end
 end
