520beda3e48ded03f2194f89eacd67af3c7235ab
Ulfmann <ulf.portner@googlemail.com>
2013-11-15 10:04:31 +0100

    reorganize methods

--- lib/video_view_price.rb
+++ lib/video_view_price.rb
@@ -23,19 +23,24 @@ class VideoViewPrice
     (sub.price / 4.0) / view_count_for(sub)
   end
 
+  def view_count_for(sub)
+    VideoView.where(subscription_id: sub.id).count
+  end
+
   def subscriptions
     Subscription.find subscription_ids
   end
 
   def subscription_ids
-    VideoView.where(video_id: video_ids, created_at: range).collect(&:subscription_id).uniq
+    watched_videos_in_range.collect(&:subscription_id).uniq
   end
 
-  def view_count_for(sub)
-    VideoView.where(subscription_id: sub.id).count
+  def watched_videos_in_range
+    VideoView.where(video_id: video_ids, created_at: range)
   end
 
   def video_ids
     Video.where(producer_id: producer_id).collect(&:id)
   end
+
 end
