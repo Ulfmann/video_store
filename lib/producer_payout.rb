fe7f5d28a1226f39f7ebd57be4972350dd8cbaec
Ulfmann <ulf.portner@googlemail.com>
2013-11-15 09:48:33 +0100

    remove duplicated code

--- lib/producer_payout.rb
+++ lib/producer_payout.rb
@@ -8,23 +8,7 @@ class ProducerPayout
     @range    = range
   end
 
-  def video_views
-    VideoView.
-      where(created_at: range).
-      where(video_id: video_ids_for_producer ).to_a
-  end
-
-  def video_ids_for_producer
-    Video.where(producer_id: producer.id).collect(&:id)
-  end
-
   def calculate
-    amount = 0
-
-    video_views.each do |vv|
-      amount += VideoViewPrice.new(vv.id, producer.id, range).value
-    end
-
-    return amount
+    VideoViewPrice.new(producer.id, range).value
   end
 end
